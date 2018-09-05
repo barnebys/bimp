1b35385675c6783818eb85cdd55cf283c8d61c71
johan <johan@baboons.se>
2018-09-05 13:57:25 +0200

    Refactoring and adding micro handler and transformers Adds Now Cloud V2.0 support

---
+++ server.js
@@ -0,0 +1,51 @@
+require("now-env");
+const { NOT_FOUND_IMAGE_URL } = process.env;
+const isUrl = require("is-url");
+const sharp = require("sharp");
+const { send } = require("micro");
+const sendImage = require("./lib/send-image");
+const { combineTransformers, createMicroHandle } = require("./lib/transform");
+const {
+  crop,
+  trim,
+  resize,
+  extract,
+  padding,
+  compress,
+  background
+} = require("./lib/transformers");
+const { proxy } = require("./lib/providers");
+
+module.exports = createMicroHandle(
+  path => {
+    if (isUrl(path)) {
+      return proxy(path);
+    } else {
+      return Promise.reject("Not implemented");
+    }
+  },
+  combineTransformers([
+    crop("crop"),
+    trim("trim"),
+    extract("extract"),
+    padding("pad"),
+    resize("w", "h"),
+    background("bg"),
+    compress()
+  ]),
+  {
+    onError: async (req, res, err) => {
+      if (NOT_FOUND_IMAGE_URL) {
+        const { contentType, file } = await proxy(NOT_FOUND_IMAGE_URL);
+        const image = sharp(file);
+        return sendImage(req, res, image, {
+          "Content-Type": contentType,
+          "Cache-Control": "public, max-age=" + 60
+        });
+      } else {
+        console.log(err);
+        return send(res, 500, "Internal error");
+      }
+    }
+  }
+);
